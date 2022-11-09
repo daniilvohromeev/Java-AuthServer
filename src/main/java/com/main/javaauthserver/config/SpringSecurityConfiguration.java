f462fbd9bf7b89283ed9e2e6d2ca492dc9b61ef2
Daniil <myps4accaunt2001@gmail.com>
2022-11-09 19:51:29 +0300

    Added html page layouts for management interface and configuration of authorization server

--- src/main/java/com/main/javaauthserver/config/SpringSecurityConfiguration.java
+++ src/main/java/com/main/javaauthserver/config/SpringSecurityConfiguration.java
@@ -2,14 +2,18 @@ package com.main.javaauthserver.config;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
+@Configuration
 public class SpringSecurityConfiguration {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -19,11 +23,19 @@ public class SpringSecurityConfiguration {
                 )
                 .logout(l-> l
                         .logoutSuccessUrl("/").permitAll())
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/");
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/java/com/main/javaauthserver/controllers/MasterController.java
+++ src/main/java/com/main/javaauthserver/controllers/MasterController.java
@@ -1,8 +1,13 @@
 package com.main.javaauthserver.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller("/")
 public class MasterController {
-
+    @GetMapping
+    public String returnAdminMenu(Model model){
+        return "settings";
+    }
 }
